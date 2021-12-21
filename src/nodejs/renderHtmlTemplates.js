b59e538e1c092daa1d8357c29098434b5e141040
Marian Poeschmann <github@mail.itsmethemojo.eu>
2021-12-21 12:39:01 +0100

    use variables in html templates

---
+++ src/nodejs/renderHtmlTemplates.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+
+require('dotenv').config();
+
+// TODO find those dynamically
+let files = ['add.html', 'index.html'];
+
+files.forEach(function(file) {
+  fs.readFile('src/' + file, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    var resultData = data;
+    Object.keys(process.env).forEach(function(key) {
+      resultData = resultData.replace('{{ ' + key + ' }}', process.env[key]);
+    });
+    fs.writeFile('dist/' + file, resultData, 'utf8', function (err) {
+       if (err) return console.log(err);
+    });
+  });
+});
