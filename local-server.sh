5bea57ef2194b3e0bf3fed092707dfc28a95a36a
Marian Poeschmann <github@mail.itsmethemojo.eu>
2021-12-21 11:33:42 +0100

    switch to go based remark api

--- local-server.sh
+++ local-server.sh
@@ -2,4 +2,4 @@
 
 docker stop remark-fe || true
 docker rm remark-fe || true
-docker run -dit --name remark-fe -v "$PWD/dist":/usr/local/apache2/htdocs/ httpd:2.4 ;echo -e "\n\n   open this url: http://"$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' remark-fe)"/\n\n"
+docker run -dit --name remark-fe -p8081:80 -v "$PWD/dist":/usr/local/apache2/htdocs/ httpd:2.4 ;echo -e "\n\n   open this url: http://localhost:8081/\n\n"
